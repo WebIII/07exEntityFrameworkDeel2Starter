549bd03669d103d2faecc05121fc14d1bcbe4bc7
ksa607 <kilytje1>
2017-11-12 07:37:51 +0100

    Add project files.

---
+++ SportsStore/Data/Mapping/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportsStore.Models;
+
+
+namespace SportsStore.Data.Mapping
+{
+    class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.HasKey(t => t.CategoryId);
+            builder.Property(t => t.Name).IsRequired().HasMaxLength(100);
+        }
+    }    
+}
